b13d47be23136513fa0e3a05fade3fb2af346c96
LilaAkuzawa <sk8erboi.0522ra@gmail.com>
2020-05-15 21:25:17 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by 阿久沢莉良 on 2020/05/15.
+//  Copyright © 2020 Chai Akuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
